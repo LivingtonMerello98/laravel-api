3629bf15b9717dbaf2d5cabcc38a26db93e49eb6
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-30 16:06:01 +0200

    [upd] Milestone 1 - api/ProjectController created
    -returns a json
    -Route to endpoint created

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+        $projects = Project::all();
+        return response()->json([
+            'success' => true,
+            'results' => $projects,
+        ]);
+    }
+}
