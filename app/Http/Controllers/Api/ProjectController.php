18bf9f3da26de6a93b35ae5ba665ae860d0bd252
LivingtonMerello98 <livington.merello@gmail.com>
2024-08-30 11:27:52 +0200

    [upd] Data Passing inclueded 'category' and 'technology' with paginate()

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,8 +11,10 @@ class ProjectController extends Controller
     public function index()
     {
         //passiamo a $project il modello Project con category e technologies relazioni stabilite nel modello Project
+        //$projects = Project::with('category', 'technologies')->get();
 
-        $projects = Project::with('category', 'technologies')->get();
+        //impaginazione tirando fuori 3 progetti per volta
+        $projects = Project::with('category', 'technologies')->paginate(3);
         return response()->json([
             'success' => true,
             'results' => $projects,
